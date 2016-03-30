c8809cd0b206b8f40890bbcbd45c4109fcc3262b
Leonardo <leosilvadev@gmail.com>
2016-03-29 23:40:01 -0300

    aula 7

--- app/server.js
+++ app/server.js
@@ -2,6 +2,9 @@ var express = require('express');
 var cors = require('cors');
 var app = express();
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://leonardo:q1w2e3@ds025379.mlab.com:25379/blogjs');
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -9,6 +12,7 @@ app.use(cors());
 var usuarioController = require('./usuario/controller');
 
 app.get('/v1/usuarios', usuarioController.listar);
+app.get('/v1/usuarios/:id', usuarioController.buscar);
 app.post('/v1/usuarios', usuarioController.cadastrar);
 app.post('/v1/usuarios/auth', usuarioController.autenticar);
 
--- app/usuario/usuarios.js
+++ app/usuario/usuarios.js
@@ -1,24 +1,56 @@
-var usuarios = [];
-var id = 0;
+var Usuario = require('./modelo');
 
-var cadastrar = function(usuario){
-  id++;
-  usuario.id = id;
-  usuarios.push(usuario);
-  return usuario;
-}
-
-var listar = function(){
-  return usuarios;
-}
-
-var autenticar = function(login, senha){
-  var autenticado = usuarios.find(function(usuario){
-    return usuario.login === login && usuario.senha === senha;
+var cadastrar = function(usuario, quandoSalvar, quandoDerErro){
+  new Usuario(usuario).save(function(err, resultados){
+      if(err){
+          quandoDerErro(err);
+      } else {
+          quandoSalvar(resultados);
+      }
   });
-  return autenticado;
+}
+
+var listar = function(quandoListar, quandoDerErro){
+    Usuario.find()
+        .select({nome:true, login:true})
+        .exec(function(err, usuarios){
+            if(err){
+                quandoDerErro(err);
+            } else {
+                quandoListar(usuarios);
+            }
+        });
+}
+
+var autenticar = function(login, senha, quandoEncontrar, quandoDerErro){
+    Usuario.findOne({login:login, senha:senha})
+        .select({nome:true, login:true})
+        .exec(function(err, usuario){
+            if(err){
+                quandoDerErro(err);
+            } else if(usuario) {
+                quandoEncontrar(usuario);
+            } else {
+                quandoDerErro(new Error('Usuario invalido!'));
+            }
+        });
+}
+
+var buscar = function(id, quandoEncontrar, quandoDerErro){
+    Usuario.findById(id)
+        .select({nome:true, login:true})
+        .exec(function(err, usuario){
+            if(err){
+                quandoDerErro(err);
+            } else if(usuario) {
+                quandoEncontrar(usuario);
+            } else {
+                quandoDerErro(new Error('Usuario invalido!'));
+            }
+        });
 }
 
 exports.cadastrar = cadastrar;
 exports.listar = listar;
 exports.autenticar = autenticar;
+exports.buscar = buscar;
