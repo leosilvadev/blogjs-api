542ef23ca0a6b0b4b7687bd42cb3eef66db2034d
Leonardo <leosilvadev@gmail.com>
2016-04-08 22:31:15 -0300

    port

--- app/server.js
+++ app/server.js
@@ -25,6 +25,6 @@ app.get('/v1/usuarios/:usuarioId/posts/:postId', postController.buscarPorDonoEId
 app.post('/v1/usuarios/:usuarioId/posts', postController.cadastrar);
 app.put('/v1/usuarios/:usuarioId/posts/:postId', postController.atualizar);
 
-app.listen(9000, function(){
+app.listen(process.env.PORT || 9000, function(){
   console.log('BlogJS API no ar...');
 });
